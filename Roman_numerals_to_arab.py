59c812a34d22681189fe1c4125766ad31b0ef758
kuba5 <Kuba50607@gmail.com>
2025-05-13 16:36:37 +0200

    updated Roman numeral converter

--- Roman_numerals_to_arab.py
+++ Roman_numerals_to_arab.py
@@ -2,24 +2,20 @@ class Solution(object):
     def romanToInt(self, s):
         output = 0
         prev_value = 0
+        roman_to_int = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
         for num in reversed(s):
-            match num:
-                case "I":
-                    cur_val = 1
-                case "V":
-                    cur_val = 5
-                case "X":
-                    cur_val = 10
-                case "L":
-                    cur_val = 50
-                case "C":
-                    cur_val = 100
-                case "D":
-                    cur_val = 500
-                case "M":
-                    cur_val = 1000
-            if output < prev_value:
+            cur_val = roman_to_int[num]
+            if cur_val < prev_value:
                 output -= cur_val
             else:
                 output += cur_val
             prev_value = cur_val
+        return output
